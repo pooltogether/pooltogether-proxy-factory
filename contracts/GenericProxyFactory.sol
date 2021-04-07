dae67923aedc818d9861d367a43d1326f3e99e33
Aodhgan <aodhgan@pooltogether.com>
2021-04-07 13:59:04 -0700

    skeleton

---
+++ contracts/GenericProxyFactory.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.6.0 <0.7.0;
+
+import "../external/openzeppelin/ProxyFactory.sol";
+
+/// @title PoolTogether Generic Minimal ProxyFactory
+/// @notice EIP-1167 Minimal proxy factory pattern for creating proxy contracts
+contract GenericProxyFactory is ProxyFactory {
+
+  /// @notice
+  constructor () public {
+    // blank?
+  }
+
+  /// @notice 
+  function create(address instance, bytes da) public returns (ATokenYieldSource) {
+    address instanceCreated = ATokenYieldSource(deployMinimal(instance, ""));
+
+    // aTokenYieldSource.initialize(_aToken, _lendingPoolAddressesProviderRegistry, _decimals, _name, _symbol);
+    // aTokenYieldSource.transferOwnership(_owner);
+
+    return instanceCreated;
+  }
+}
---
+++ test/GenericProxyFactory.test.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('GenericProxyFactory', () => {
+  describe('create()', () => {
+    it('should create a new instance', async () => {
+      const provider = ethers.provider;
+
+      const genericProxyFactoryContract = await ethers.getContractFactory(
+        'GenericProxyFactory',
+      );
+
+      const hardhatGenericProxyFactory = await genericProxyFactoryContract.deploy()
+
+      const tx = await hardhatGenericProxyFactory.create(
+        '0x3A791e828fDd420fbE16416efDF509E4b9088Dd4',
+        ''
+      );
+      const receipt = await provider.getTransactionReceipt(tx.hash);
+      const createdEvent = hardhatGenericProxyFactory.interface.parseLog(receipt.logs[0]);
+
+      expect(createdEvent.name).to.equal('ProxyCreated');
+
+      // to do add instance specific tests
+
+    });
+  });
+});
