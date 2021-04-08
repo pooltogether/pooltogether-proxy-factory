ea0a45296f15c74a2d59e0dfcf16ff42710326f2
Aodhgan <aodhgan@pooltogether.com>
2021-04-08 15:30:04 -0700

    refactor

--- contracts/GenericProxyFactory.sol
+++ contracts/GenericProxyFactory.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/ClonesUpgradeable.sol";
-import "hardhat/console.sol";
 
 /// @title PoolTogether Generic Minimal ProxyFactory
 /// @notice EIP-1167 Minimal proxy factory pattern for creating proxy contracts
@@ -15,37 +13,32 @@ contract GenericProxyFactory{
   /// @notice Create a proxy contract for given instance
   /// @param _instance Contract implementation which the created contract will point at
   /// @param _data Data which is to be called after the proxy contract is created
-  function create(address _instance, bytes calldata _data) public
-  returns (address instanceCreate) {
+  function create(address _instance, bytes calldata _data) public returns (address instanceCreated, bytes memory result) {
     
-    address instanceCreated = ClonesUpgradeable.clone(_instance);
+    instanceCreated = ClonesUpgradeable.clone(_instance);
     emit ProxyCreated(instanceCreated, _instance);
 
-    console.log("now calling passed data ");
-
     if(_data.length > 0) {
-      (bool success, bytes memory result) = instanceCreated.call(_data);
-      require(success, "GenericProxyFactory/call-failed");
+      return callContract(instanceCreated, _data);
     }
 
-    return instanceCreated;
+    return (instanceCreated, "");  
   }
 
   /// @notice Create a proxy contract with a deterministic address using create2
   /// @param _instance Contract implementation which the created contract will point at
   /// @param _salt Salt which is used as the create2 salt
   /// @param _data Data which is to be called after the proxy contract is created
-  function create2(address _instance, bytes32 _salt, bytes calldata _data) public returns (address) {
+  function create2(address _instance, bytes32 _salt, bytes calldata _data) public returns (address instanceCreated, bytes memory result) {
 
-    address instanceCreated = ClonesUpgradeable.cloneDeterministic(_instance, _salt);
+    instanceCreated = ClonesUpgradeable.cloneDeterministic(_instance, _salt);
     emit ProxyCreated(instanceCreated, _instance);
 
     if(_data.length > 0) {
-      (bool success,) = instanceCreated.call(_data);
-      require(success, "GenericProxyFactory/call-failed");
+      return callContract(instanceCreated, _data);
     }
 
-    return instanceCreated;
+    return (instanceCreated, "");
   }
 
   /// @notice Calculates what the proxy address would be when deterministically created
@@ -53,6 +46,16 @@ contract GenericProxyFactory{
   /// @param _salt Salt which would be used as the create2 salt
   function predictDeterministicAddress(address _master, bytes32 _salt) public view returns (address) {
     return ClonesUpgradeable.predictDeterministicAddress(_master, _salt, address(this));
+  }
+
+  /// @notice Calls the instance contract with the specified data
+  /// @dev Will revert if call unsuccessful 
+  /// @param target Call target contract
+  /// @param _data Data for contract call
+  function callContract(address target, bytes memory _data) internal returns (address, bytes memory) {
+    (bool success, bytes memory returnData) = target.call(_data);
+    require(success, string(returnData));
+    return (target, returnData);
   }
 
 }
